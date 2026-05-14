adc0db2897b1d3d98fec1d033d5d97243c253607
praneshkA <pranesh.ka2023it@sece.ac.in>
2026-05-15 00:17:03 +0530

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { useState } from 'react';
 import Dashboard from './pages/Dashboard';
 import Products from './pages/Products';
 
-import AdminLogin from './components/AdminLogin';
+import AdminLogin from './components/AdminLogin.jsx';
 
 function App() {
 
